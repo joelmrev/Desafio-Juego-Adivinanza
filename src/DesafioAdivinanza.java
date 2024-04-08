8b1c0f9907fe6cf3943c25aa643b66c570d2706b
joelmrev <joelmrev@gmail.com>
2024-04-07 23:50:20 -0600

    ADD: Desafío Concluido

--- src/DesafioAdivinanza.java
+++ src/DesafioAdivinanza.java
@@ -1,6 +1,37 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class DesafioAdivinanza {
 
     public static void main(String[] args) {
 
+        Scanner teclado = new Scanner(System.in);
+        int numeroAleatorio = new Random().nextInt(100);
+        int numeroUsuario = 0;
+        int contador = 0;
+        System.out.println(numeroAleatorio);
+
+        for (int i = 0; i < 5 ; i++) {
+            System.out.println((i+1) + " Ingresa un número del 1 al 100: ");
+            numeroUsuario = teclado.nextInt();
+            contador++;
+
+            if (numeroUsuario != numeroAleatorio) {
+                if (numeroUsuario > numeroAleatorio){
+                    System.out.println("El número es MENOR. \n");
+                }else {
+                    System.out.println("El número es MAYOR. \n");
+                }
+            } else {
+                System.out.println("EXCELENTE!, el número aleatorio es: " + numeroAleatorio );
+                System.out.println("Número de intentos: " + contador);
+                break;
+            }
+
+            if (contador == 5) {
+                System.out.println("LLEGO AL NÚMERO MÁXIMO DE INTENTOS. \n");
+            }
+
+        }
     }
 }
